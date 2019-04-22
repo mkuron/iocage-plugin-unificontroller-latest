3929f2809877c7657d2d5a57ad2be1af1cb702aa
Michael Kuron <m.kuron@gmx.de>
2019-04-22 10:05:27 +0200

    installation

--- post_install.sh
+++ post_install.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+
+set VERSION=5.10.21
+
+portsnap fetch
+portsnap extract
+cd /usr/ports/net-mgmt/unifi5/
+sed -i '' "s/^PORTVERSION=.*/PORTVERSION=$VERSION/g" Makefile
+make makesum
+make install
+make clean
+
+sysrc -f /etc/rc.conf unifi_enable="YES"
+service unifi start 2>/dev/null
